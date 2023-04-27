fe739103714c762a87974eca5a23168dcc25e9f1
xdiegocs <dctrs.gutierrez@gmail.com>
2023-04-27 13:16:11 -0600

    se presentan los ejercicios de la sec 3

---
+++ Seccion3/Ejercicio28.4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu nombre: ");
+            String name = Console.ReadLine();
+            Console.Write("\nHola, " + name);
+            Console.ReadKey();
+        }
+    }
+}

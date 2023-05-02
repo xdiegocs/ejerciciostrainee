ea94ebb85a07252b41b3bd308d66a4e3b129a2c0
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-02 11:32:30 -0600

    Se presenta la solucion del reto de la Semana 1

---
+++ retoSemana1/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Ejercicio_Semana1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Ingresa un numero: "); //el usuario ingresara un numero
+            int number = int.Parse(Console.ReadLine()); //aqui se almacena el numero
+
+            //numero mayor a 100
+            if (number > 100) //se valida si el numero es mayor a 100 mediante el operador
+            {
+                Console.WriteLine("El numero ingresado es mayor a 100");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es menor o igual a 100");
+            }
+
+            //numero par/impar
+            if (number % 2 == 0) //si el numero mediante el resto con el 2 es igual a cero entonces es par 
+            {
+                Console.WriteLine("El numero ingresado es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es impar");
+            }
+
+            //FOR
+            //numero primo
+            bool numeroPrimo = true; //se estable la variable como "true"
+            for (int i = 2; i < number; i++) //se evalua un ciclo
+            {
+                if (number % 2 == 0)
+                {
+                    numeroPrimo = false; //si cumple la condicion pasa a tener el valor "false"
+                    break; //entonces termina la condicion
+                }
+            }
+            if (numeroPrimo) //se valida y se imprime mediante la condicion anterior
+            {
+                Console.WriteLine("El numero ingresado es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado no es primo");
+            }
+
+            /*//IF-ELSE
+            //numero primo
+            bool numeroPrimo = true; //se estable la variable como "true"
+
+            if (number % 2 == 0)
+            {
+                numeroPrimo = false; //si el numero es "false" quiere decir que no lo es
+                Console.WriteLine("El numero ingresado no es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es primo");
+            }*/
+            
+        }
+    }
+}

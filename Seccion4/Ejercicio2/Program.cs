314a330e7e47d9f11af140f5caeecc728205dcd0
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 11:58:51 -0600

    se presenta el ejercicio 2 de la seccion 4

---
+++ Seccion4/Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Seccion4_Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa el primer numero: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingresa el segundo numero: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            //validacion de la condicion de los numeros anteriormente ingresados
+            if (numero1 > numero2)
+            {
+                Console.WriteLine(" " + numero1 + " es mayor que " + numero2);
+            }
+            else
+            {
+                Console.WriteLine(" " + numero2 + " es mayor que " + numero1);
+            }
+        }
+    }
+}

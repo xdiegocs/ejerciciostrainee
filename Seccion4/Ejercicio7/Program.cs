867cbdb855f98bfe4e81ad361386737e3ec6c261
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:04:18 -0600

    se presenta el ejercicio 7 de la seccion 4

---
+++ Seccion4/Ejercicio7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Seccion4_Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //se usara un bucle for para este ejercicio
+            for (int i = 1; i <= 100; i++)
+            {
+                //se define una condicion donde se usa el resto para encontrar los numeros pares
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

2815441ec54a1ed98d45a5f96168db3b74cb69c1
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 11:55:41 -0600

    se presenta la solucion del ejercicio 1 de la seccion 4

---
+++ Seccion4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Seccion4_Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion de variables
+            int numero1 = 5;
+            int numero2 = 10;
+            int numero3 = 15;
+            int sumaDeNumeros = numero1 + numero2 + numero3;
+
+            Console.WriteLine("El resultado de la suma de los tres numeros es: " + sumaDeNumeros);
+            Console.ReadKey();
+        }
+    }
+}

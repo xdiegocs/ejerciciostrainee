8eea7cbd159ad3b276b8944063a6c0e5d1cd1e49
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:08:46 -0600

    se presenta el ejercicio 13 de la seccion 4

---
+++ Seccion4/Ejercicio13/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Seccion4_Ejercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            bool numeroPrimo = true; //se estable la variable como "true"
+            for (int i = 2; i < numero; i++) //se evalua un ciclo
+            {
+                if (numero % 2 == 0)
+                {
+                    numeroPrimo = false; //si cumple la condicion pasa a tener el valor "false"
+                    break; //entonces termina la condicion
+                }
+            }
+            if (numeroPrimo) //se valida y se imprime mediante la condicion anterior
+            {
+                Console.WriteLine("El numero ingresado es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado no es primo");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

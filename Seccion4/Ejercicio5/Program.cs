9e53bcc5e9e852673e76b82d974d9368f0e49095
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:02:13 -0600

    se presenta el ejercicio 5 de la seccion 4

---
+++ Seccion4/Ejercicio5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Seccion4_Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //este conteo se realizara del 1 al 100
+            for (int i = 1;  i <= 100; i++)
+            {
+                
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

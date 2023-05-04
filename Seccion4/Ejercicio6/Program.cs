00ac139fe1de0d5882614a46e1de6d3ae2df3327
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:03:21 -0600

    se presenta el ejercicio 6 de la seccion 4

---
+++ Seccion4/Ejercicio6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Seccion4_Ejercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //se declara la variable que comenzara desde 1
+            int i = 1;
+
+            while (i <= 100) 
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}

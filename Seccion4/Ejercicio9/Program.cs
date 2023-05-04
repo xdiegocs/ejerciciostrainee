bc80997bd7e279e3e56610f3d02015e592d708b6
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:05:45 -0600

    se presenta el ejercicio 9 de la seccion 4

---
+++ Seccion4/Ejercicio9/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Seccion4_Ejercicio9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //se declara el array
+            int[] numerosPares = new int[50];
+            int index = 0;
+
+            //por medio del bucle se insertan los numeros pares
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    numerosPares[index] = i;
+                    index++;
+                }
+            }
+
+            Console.WriteLine("Numero pares del 1 al 100");
+
+            //posteriormente se imprimen mediante foreach accediendo a cada numero
+            foreach (int numero in numerosPares)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}

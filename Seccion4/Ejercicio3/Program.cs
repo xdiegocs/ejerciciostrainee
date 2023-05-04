6f2d31a658f635a05e5b727e4a63944d39a76003
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:00:11 -0600

    se presenta el ejercicio 3 de la seccion 4

---
+++ Seccion4/Ejercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Seccion4_Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir el nombre de la semana al usuario
+            Console.WriteLine("Ingrese el nombre de la semana: ");
+            string diaSemana = Console.ReadLine().ToLower();
+
+            //se verificara si es fin de semana o no
+            if (diaSemana == "sabado" || diaSemana == "domingo")
+            {
+                Console.WriteLine("Es fin de semana");
+            }
+            else
+            {
+                Console.WriteLine("No es fin de semana");
+            }
+            
+        }
+    }
+}

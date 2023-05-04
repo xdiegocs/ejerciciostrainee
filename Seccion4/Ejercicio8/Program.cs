1796e503c8fa58e8cb70d373622388427e3595a5
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:05:05 -0600

    se presenta el ejercicio 8 de la seccion 4

---
+++ Seccion4/Ejercicio8/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Seccion4_Ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                //en cada iteracion se verifica si "i" es par o divisible entre 3
+                //por eso se utiliza el operador "OR = ||"
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

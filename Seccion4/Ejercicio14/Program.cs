cfe71fc31ed3abd141c9fff36818a4510a3fe6f5
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-04 12:09:33 -0600

    se presenta el ejercicio 14 de la seccion 4

---
+++ Seccion4/Ejercicio14/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Seccion4_Ejercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cifras = 0;
+
+            Console.Write("Ingrese un numero entero positivo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            //se valida por medio del indice del numero
+            if (numero == 0)
+            {
+                cifras = 1;
+            }
+            else
+            {
+                //se utiliza el bucle para ir dividiendo el numero entre 10 y aumentar la variable
+                while (numero > 0) 
+                {
+                    cifras++;
+                    numero /= 10;
+                }
+            }
+
+            Console.WriteLine("El numero tiene {0} cifras.", cifras);
+        }
+    }
+}

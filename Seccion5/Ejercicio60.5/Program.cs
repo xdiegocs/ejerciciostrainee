c9474db09ebbef6b6c73e98e4746237a0c1837cf
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-07 08:24:47 -0600

    se presenta la solucion del ejercicio 5 seccion 5

---
+++ Seccion5/Ejercicio60.5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Seccion5_Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            ImprimirTablaMultiplicar(numero);
+
+            Console.ReadLine();
+        }
+
+        static void ImprimirTablaMultiplicar(int numero)
+        {
+            Console.WriteLine($"\nTabla de multiplicar del numero {numero}:\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+        }
+    }
+}

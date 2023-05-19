9fcc2b0792c387c95d5960dfdff92f39e15a49df
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-19 10:20:41 -0600

    se presenta la solucion del ejercicio de la seccion 10

---
+++ Semana4/Seccion10/Ejercicio108.1/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion10_Ejercicio1
+{
+    internal class Animal : IAnimal
+    {
+        public void Andar()
+        {
+            Console.WriteLine("El animal está andando");
+        }
+
+        public bool EsPerro()
+        {
+            return true;
+        }
+
+        public void Saltar()
+        {
+            Console.WriteLine("El animal está saltando");
+        }
+    }
+}
---
+++ Semana4/Seccion10/Ejercicio108.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Seccion10_Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Andar();
+
+            if (animal.EsPerro())
+            {
+                Console.WriteLine("El animal es un perro");
+            }
+            else
+            {
+                Console.WriteLine("El animal no es un perro");
+            }
+
+            animal.Saltar();
+            Console.ReadKey();
+        }
+    }
+}

58410c73e1dff3f3e3f2a28cd48becc12f1de29c
xdiegocs <dctrs.gutierrez@gmail.com>
2023-05-30 10:48:37 -0600

    se presenta la solucion del ejercicio de la seccion 14

---
+++ Semana6/Seccion14/Ejercicio147.1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Seccion14_Ejercicio1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Comenzando la receta...");
+
+            Receta receta = new Receta();
+            await receta.HacerTortilla();
+
+            Console.WriteLine("Receta terminada.");
+        }
+    }
+}
---
+++ Semana6/Seccion14/Ejercicio147.1/Receta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion14_Ejercicio1
+{
+    public class Receta
+    {
+        public async Task HacerTortilla()
+        {
+            await PelarPatatas();
+            await CortarPatatas();
+            await BatirHuevos();
+            await MezclarIngredientes();
+            await CocinarTortilla();
+        }
+
+        private async Task PelarPatatas()
+        {
+            Console.WriteLine("Pelando las patatas...");
+            await Task.Delay(2000);
+            Console.WriteLine("Patatas peladas.");
+        }
+
+        private async Task CortarPatatas()
+        {
+            Console.WriteLine("Cortando las patatas...");
+            await Task.Delay(1500);
+            Console.WriteLine("Patatas cortadas.");
+        }
+
+        private async Task BatirHuevos()
+        {
+            Console.WriteLine("Batiendo los huevos...");
+            await Task.Delay(1000);
+            Console.WriteLine("Huevos batidos.");
+        }
+
+        private async Task MezclarIngredientes()
+        {
+            Console.WriteLine("Mezclando las patatas con los huevos...");
+            await Task.Delay(2000);
+            Console.WriteLine("Ingredientes mezclados.");
+        }
+
+        private async Task CocinarTortilla()
+        {
+            Console.WriteLine("Cocinando la tortilla...");
+            await Task.Delay(3000);
+            Console.WriteLine("¡La tortilla está lista!");
+        }
+    }
+}

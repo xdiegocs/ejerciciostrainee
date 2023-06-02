a0b840184c9d2e8e6efa4882c04bb5049b3eea6d
xdiegocs <dctrs.gutierrez@gmail.com>
2023-06-01 19:51:33 -0600

    se presenta la solucion del reto de la semana 5

---
+++ Semana5/RetoSemana5/Jugador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Semana5
+{
+    [Serializable]
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public int Nivel { get; set; }
+        public int PuntosVida { get; set; }
+        public int NumeroReinicios { get; set; }
+
+        public void GuardarDatos(string rutaArchivo)
+        {
+            try
+            {
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                using (FileStream fileStream = new FileStream(rutaArchivo, FileMode.Create))
+                {
+                    bFormatter.Serialize(fileStream, this);
+                    Console.WriteLine("Los datos se han guardado exitosamente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al guardar los datos del jugador.");
+                RegistrarErrores(ex);
+            }
+        }
+
+        public static Jugador CargarDatos(string rutaArchivo)
+        {
+            try
+            {
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                using (FileStream fileStream = new FileStream(rutaArchivo, FileMode.Open))
+                {
+                    Jugador jugador = (Jugador)bFormatter.Deserialize(fileStream);
+                    Console.WriteLine("Los datos se han cargado exitosamente.");
+                    return jugador;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al cargar los datos del jugador.");
+                RegistrarErrores(ex);
+                return null;
+            }
+        }
+
+        private static void RegistrarErrores(Exception ex)
+        {
+            string mensajeError = $"[{DateTime.Now}] Error: {ex.Message}";
+            File.AppendAllText("ErroresJugador/registro_errores.txt", mensajeError + Environment.NewLine);
+        }
+    }
+}
---
+++ Semana5/RetoSemana5/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Ejercicio_Semana5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenido a un emocionante e intrigante juego de aventuras!");
+
+            Console.Write("Ingrese el nombre del jugador: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese el nivel del jugador: ");
+            int nivel = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese los puntos de vida del jugador: ");
+            int puntosVida = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número de reinicios del jugador: ");
+            int numeroReinicios = int.Parse(Console.ReadLine());
+
+            Jugador jugador = new Jugador
+            {
+                Nombre = nombre,
+                Nivel = nivel,
+                PuntosVida = puntosVida,
+                NumeroReinicios = numeroReinicios
+            };
+
+            Console.Write("Desea guardar los datos del jugador (S/N): ");
+            string respuesta = Console.ReadLine();
+
+            if (respuesta.ToUpper() == "S")
+            {
+                string rutaArchivo = "DatosJugador/datosjugador.bin";
+
+                jugador.GuardarDatos(rutaArchivo);
+            }
+
+            Console.Write("Desea cargar los datos del jugador (S/N): ");
+            respuesta = Console.ReadLine();
+
+            if (respuesta.ToUpper() == "S")
+            {
+                string rutaArchivo = "DatosJugador/datosjugador.bin";
+
+                Jugador cargarJugador = Jugador.CargarDatos(rutaArchivo);
+
+                if (cargarJugador != null)
+                {
+                    Console.WriteLine($"Nombre: {cargarJugador.Nombre}");
+                    Console.WriteLine($"Nivel: {cargarJugador.Nivel}");
+                    Console.WriteLine($"Puntos de Vida: {cargarJugador.PuntosVida}");
+                    Console.WriteLine($"Número de Reinicios: {cargarJugador.NumeroReinicios}");
+                }
+            }
+
+            Console.WriteLine("Fin del programa.");
+            Console.ReadKey();
+        }
+    }
+}

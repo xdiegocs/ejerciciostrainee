70cbb6c53b382b0a82f5ad143511543d3b986f3d
xdiegocs <dctrs.gutierrez@gmail.com>
2023-06-23 10:04:58 -0600

    se presenta la solucion del reto semanal

---
+++ Semana8/RetoSemana8/IFlightManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Semana8
+{
+    public interface IFlightManager
+    {
+        void CargarDatosDeCSV();
+        void InsertDataIntoTable(List<Flight> vuelos);
+        List<string> ObtenerDestinoPorOrigen(string origen);
+        List<string> ObtenerOrigenPorDestino(string destino);
+    }
+}
